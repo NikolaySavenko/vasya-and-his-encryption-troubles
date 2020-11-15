4957fe9c66b49af0a9fa17d145667d3cfae3e25f
Nikolay Savenko <savenkons@yandex.ru>
2020-11-16 00:19:23 +0300

    main file change and some refactoring

---
+++ bot_run.py
@@ -0,0 +1,18 @@
+import telebot
+import os
+
+bot = telebot.TeleBot(os.environ.get('API_KEY_TELEGRAM'))
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+	bot.send_message(message.chat.id, "Omae wa mou shindeiru")
+
+
+@bot.message_handler(content_types=['text'])
+def message_listener(message):
+	bot.send_message(message.chat.id, 'Oh no, im still broken, but im working!')
+
+
+# RUN
+bot.polling(none_stop=True)
