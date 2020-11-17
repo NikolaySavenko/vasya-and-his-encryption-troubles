74ccb4c4336676205d019ad1700e2f64c9d8c565
VetaSolo <vetasolo75@gmail.com>
2020-11-17 12:19:46 +0300

    First vers. encryption alg.

--- encoder.py
+++ encoder.py
@@ -1,11 +1,28 @@
+from func import txt_to_num, convert_base, alohomora, code
+
+
 def encode(word: str) -> str:
 	"""
 	:param word: just a word which should be encoded to key
 	:return: encoded key
 	"""
 	print(f"encoding from {word}")
-	key = word[::-1]
-	return key
+	text_ch = code(word)  # в шифр Цезаря
+	chisl = txt_to_num(text_ch)  # 10СС
+	cod = alohomora(chisl)
+
+	if len(cod) % 2 == 1:
+		x = 0
+		for i in range(len(cod)):
+			x += int(cod[i])
+		cod.append(x)
+	N = len(cod)
+	cod_2 = [0] * (N // 2)
+	for i in range(N // 2):
+		cod_2[i] = abs(int(cod[i], 10) - int(cod[N - 1 - i]))
+	for i in range(len(cod_2)):
+		cod_2[i] = cod_2[i] = convert_base(cod_2[i], to_base=6, from_base=16)
+	return "".join(cod_2)
 
 
 # deprecated
---
+++ func.py
@@ -0,0 +1,41 @@
+def Text2(text): #каждый символ в двоичный код
+    text_2 = [bin(c)[2:].rjust(8, '0') for c in text.encode('utf-8')]
+    return ''.join(text_2)
+
+def txt_to_num(letter):
+    t = []
+    for i in range(len(letter)):
+        t.append((ord(letter[i].upper()) - 1039))
+    return t
+def convert_base(num, to_base=10, from_base=10):
+    if isinstance(num, str):
+        n = int(num, from_base)
+    else:
+        n = int(num)
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if n < to_base:
+        return alphabet[n]
+    else:
+        return convert_base(n // to_base, to_base) + alphabet[n % to_base]
+def alohomora(mass):
+    for i in range(len(mass)):
+        if mass[i] % 2 == 0:
+            mass[i] = convert_base(mass[i], to_base=4)
+        elif mass[i] % 3 == 0:
+            mass[i] = convert_base(mass[i], to_base=9)
+        else:
+            mass[i] = convert_base(mass[i], to_base=5)
+    return mass
+def code(text): #что-то типа шифра Цезаря
+    #import random
+    ALPHA = u'абвгдеёжзийклмнопрстуфхцчшщьъэюя'
+    #step = random.randint(2, 15)
+    if len(text) > 4:
+        step = len(text)//4
+    else:
+        step = len(text)*4//2
+    return text.translate(
+        str.maketrans(ALPHA, ALPHA[step:] + ALPHA[:step]))
+def decode(text, step):
+    return text.translate(
+        str.maketrans(ALPHA[step:] + ALPHA[:step], ALPHA))
